ddbe89493e3059e37da5725727ccb1ae269ee20b
Sam D <samidiaz3@gmail.com>
2021-01-07 17:41:36 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,7 +5,7 @@
 # set a counter
 # iterate over each board array element to see if x or o, and if so we increment counter by 1
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", "X", "X", " ", " ", " "]
 counter = 0
 board.each do |board|
   if board == "X" || board == "O"
