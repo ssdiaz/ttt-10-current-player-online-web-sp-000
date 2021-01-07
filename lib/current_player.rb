95e2b3b36e5b6ea03bad717849b309c82314b2f5
Sam D <samidiaz3@gmail.com>
2021-01-07 18:10:41 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@
 
 def turn_count (board)
   counter = 0
-  board.each do |board|
-    if board == "X" || board == "O" 
-      counter = counter + 1
+  board.each do |index|
+    if index == "X" || index == "O"
+      counter += 1
     end
   end
   puts counter
