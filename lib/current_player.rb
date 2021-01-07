fd1446c66c8cf7889f1caf0359b7d12cefcdd13f
Sam D <samidiaz3@gmail.com>
2021-01-07 17:30:19 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,15 @@
-def turn_count (board)
-#returns number of turns played
+# takes in board argument
+# returns number of turns that have been played
+# X goes first.
+# use #each method
+# set a counter
+# iterate over each board array element to see if x or o, and if so we increment counter by 1
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+counter = 0
+board.each do |board|
+  if board[index] = "X" || board[index] = "O"
+    puts "yes"
+    counter += 1
+  end
 end
